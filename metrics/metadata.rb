6fc5d3f93325727383c9fe7a7eaf24f1fa4f0421
jhert <jeroen@distimo.com>
2014-06-19 16:30:16 +0200

    Added custom amazon cruft

---
+++ metrics/metadata.rb
@@ -0,0 +1,8 @@
+name             'metrics'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures metrics'
+long_description 'Installs/Configures metrics'
+version          '0.1.0'
+
